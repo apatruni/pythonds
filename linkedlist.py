02348bde62edbc4d1d60ee5ca6d33e2388a5087f
Aditya Patruni <adityapatuni@gmail.com>
2024-01-26 11:08:09 -0600

    first

---
+++ linkedlist.py
@@ -0,0 +1,65 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def __str__(self):
+        return f"{self.data}"
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = Node(-1)
+        self.size = 0
+        self.tail = self.head
+    
+    def insertAtEnd(self, data):
+        self.tail.next = Node(data)
+        self.tail = self.tail.next
+        self.size += 1
+    
+    def insertAtHead(self, data):
+        remainingListHead = self.head.next
+        self.head.next = Node(data)
+        self.head.next.next = remainingListHead
+        self.size += 1
+
+    def insertAtPos(self, pos, data):
+        if self.size < pos:
+            return
+
+        currPos = 1
+        currPtr = self.head
+
+        while currPos < pos:
+            currPtr = currPtr.next
+            currPos += 1
+        
+        restOfListHead = currPtr.next
+        currPtr.next = Node(data)
+        currPtr.next.next = restOfListHead
+
+    def deleteAll(self):
+        self.head.next = None
+
+    def deleteStartingFromPos(self,pos):
+        currPos = 1
+        currPtr = self.head
+
+        while currPos < pos:
+            currPtr = currPtr.next
+            currPos += 1
+        
+        currPtr.next = None
+
+    def __repr__(self):
+        currPtr = self.head.next
+        repr = ""
+        while currPtr:
+            repr += str(currPtr.data) + " , "
+            currPtr = currPtr.next
+        return repr[:-2]
+    
+
+
+
